2dbe90da91fdf43471c1582ddc0fd33d238f58b1
Troy Nelson <troymn625@gmail.com>
2025-03-13 20:31:07 -0500

    added buttons and array

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 
 const submitBtn = document.getElementById('submitBtn');
 const inputEl = document.getElementById('todo-input');
+const todosUlEl = document.getElementById('todos');
+
+let todos = [];
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const todosUlEl = document.getElementById('todos');
-  const newEl = document.createElement('li');
-  newEl.textContent = inputEl.value;
-  console.log(newEl);
+  let newEl = document.createElement('li');
+  let delBtn = document.createElement('button');
+  delBtn.innerText = "Delete";
+  delBtn.setAttribute("class", "delete");
+  let editBtn = document.createElement('button');
+  editBtn.innerText = "Edit";
+  editBtn.setAttribute("class", "edit");
+  newEl.innerText = inputEl.value;
+  newEl.appendChild(delBtn);
+  newEl.appendChild(editBtn);
+  todos.push(newEl.innerText);
   todosUlEl.appendChild(newEl);
-
 });
+
+deleteBtn = document.getElementsByClassName('delete');
+
+deleteBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(e);
+})
